fb1bb7f733a94e9b7967f465bd7abdc04d6903c7
Ksao0 <128844313+Ksao0@users.noreply.github.com>
2023-04-02 23:10:06 +0200

    Add files via upload

---
+++ Aktualizacja.py
@@ -0,0 +1,15 @@
+import os
+import urllib.request
+
+# ścieżka do pliku main.py w bieżącym folderze
+path = os.path.join(os.getcwd(), "main.py")
+
+# usuń plik main.py, jeśli istnieje
+if os.path.exists(path):
+    os.remove(path)
+print("Usunięto plik main.py")
+
+# pobierz plik main.py z repozytorium
+url = "https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/main.py"
+urllib.request.urlretrieve(url, path)
+print("Zastąpiono plik")

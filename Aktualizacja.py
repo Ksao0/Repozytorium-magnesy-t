553dc85971c4ef7b71a8578a36f1b1777a1cba44
Ksao0 <128844313+Ksao0@users.noreply.github.com>
2023-04-03 18:32:46 +0200

    Update Aktualizacja.py

--- Aktualizacja.py
+++ Aktualizacja.py
@@ -1,6 +1,6 @@
 import os
 import urllib.request
-
+import datetime
 # ścieżka do pliku main.py w bieżącym folderze
 path = os.path.join(os.getcwd(), "main.py")
 
@@ -8,9 +8,46 @@ path = os.path.join(os.getcwd(), "main.py")
 if os.path.exists(path):
     os.remove(path)
 print("Usunięto plik main.py")
-
 # pobierz plik main.py z repozytorium
 url = "https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/main.py"
 urllib.request.urlretrieve(url, path)
-print("Zastąpiono plik")
+print("Zastąpiono plik main.py")
+
+
+# ścieżka do pliku version.txt w bieżącym folderze
+path = os.path.join(os.getcwd(), "version.txt")
+
+# zapisz zawartość pliku version.txt do zmiennej stara_version
+if os.path.exists(path):
+    with open(path, "r") as f:
+        stara_version = f.read()
+else:
+    stara_version = ""
+
+# usuń plik version.txt, jeśli istnieje
+if os.path.exists(path):
+    os.remove(path)
+print("Usunięto plik version.txt")
+
+# pobierz plik version.txt z repozytorium
+url = "https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/version.txt"
+urllib.request.urlretrieve(url, path)
+print("Zastąpiono plik version.txt")
+
+# odczytaj zawartość pliku version.txt do zmiennej nowa_version
+with open(path, "r") as f:
+    nowa_version = f.read()
+
+now = datetime.datetime.now()
+data_obliczenia = now.strftime("%d.%m.%Y %H:%M")
+
+with open("Zapisy.txt", "a") as plik:
+    plik.write(
+        f"          Zaktualizowano program do nowej wersji! data: {data_obliczenia}\n")
+    plik.write(f"           Stara wersja: {stara_version}\n")
+    plik.write(f"           Nowa wersja: {nowa_version}\n\n")
+
+print(f"Stara wersja: {stara_version}\n")
+print(f"Nowa wersja: {nowa_version}\n\n")
+
 input("Naciśnij klawisz Enter, aby zakończyć program...")

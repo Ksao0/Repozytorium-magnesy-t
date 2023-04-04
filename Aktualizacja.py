63bb0238b966623158c461a6746a8c12e21ef575
Ksao0 <128844313+Ksao0@users.noreply.github.com>
2023-04-04 15:26:20 +0200

    Update Aktualizacja.py

--- Aktualizacja.py
+++ Aktualizacja.py
@@ -13,6 +13,38 @@ url = "https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/main.
 urllib.request.urlretrieve(url, path)
 print("Zastąpiono plik main.py")
 
+# NOWE FUNKCJE
+
+### Kasowanie Uruchamianie.py
+
+# nazwa pliku
+nazwa_pliku = 'Uruchamianie.py'
+
+# usuń plik o nazwie 'Uruchamianie.py', jeśli taki istnieje
+if os.path.exists(nazwa_pliku):
+    os.remove(nazwa_pliku)
+    print(f'Usunięto plik {nazwa_pliku}.')
+
+### Koniec dla: Kasowanie Uruchamianie.py
+
+### Aktualizacja pliku Aktualizator_aktualizatora
+
+# ścieżka do pliku Aktualizator_aktualizatora.py w bieżącym folderze
+path = os.path.join(os.getcwd(), "Aktualizator_aktualizatora.py")
+
+# usuń plik Aktualizator_aktualizatora.py, jeśli istnieje
+if os.path.exists(path):
+    os.remove(path)
+print("Usunięto plik Aktualizator_aktualizatora.py")
+# pobierz plik main.py z repozytorium
+url = "https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/Aktualizator_aktualizatora.py"
+urllib.request.urlretrieve(url, path)
+print("Zastąpiono plik Aktualizator_aktualizatora.py")
+
+### Koniec dla: Aktualizacja pliku Aktualizator_aktualizatora
+
+# KONIEC NOWYCH FUNKCJI
+
 # ścieżka do pliku version.txt w bieżącym folderze
 path = os.path.join(os.getcwd(), "version.txt")
 
@@ -52,7 +84,8 @@ print(f"Nowa wersja: {nowa_version}\n\n")
 
 # NOWE FUNKCJE
 
-# Kasowanie Uruchamianie.py
+### Kasowanie Uruchamianie.py
+
 # nazwa pliku
 nazwa_pliku = 'Uruchamianie.py'
 
@@ -61,9 +94,9 @@ if os.path.exists(nazwa_pliku):
     os.remove(nazwa_pliku)
     print(f'Usunięto plik {nazwa_pliku}.')
 
-# Koniec dla: Kasowanie Uruchamianie.py
+### Koniec dla: Kasowanie Uruchamianie.py
 
-# Aktualizacja pliku Aktualizator_aktualizatora
+### Aktualizacja pliku Aktualizator_aktualizatora
 
 # ścieżka do pliku Aktualizator_aktualizatora.py w bieżącym folderze
 path = os.path.join(os.getcwd(), "Aktualizator_aktualizatora.py")
@@ -77,7 +110,7 @@ url = "https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/Aktua
 urllib.request.urlretrieve(url, path)
 print("Zastąpiono plik Aktualizator_aktualizatora.py")
 
-# Koniec dla: Aktualizacja pliku Aktualizator_aktualizatora
+### Koniec dla: Aktualizacja pliku Aktualizator_aktualizatora
 
 # KONIEC NOWYCH FUNKCJI
 

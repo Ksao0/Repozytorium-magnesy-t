de66a228dee96785facd832a2765cfb4a8e28d75
Ksao0 <128844313+Ksao0@users.noreply.github.com>
2023-04-04 14:57:19 +0200

    Update Aktualizacja.py

--- Aktualizacja.py
+++ Aktualizacja.py
@@ -51,24 +51,12 @@ print(f"Nowa wersja: {nowa_version}\n\n")
 
 # NOWA FUNKCJA
 
-# nazwa pliku do utworzenia
 nazwa_pliku = 'Uruchamianie.py'
 
 # usuń plik o nazwie 'Uruchamianie.py', jeśli taki istnieje
 if os.path.exists(nazwa_pliku):
     os.remove(nazwa_pliku)
     print(f'Usunięto plik {nazwa_pliku}.')
-
-# otwórz plik w trybie zapisu
-with open(nazwa_pliku, 'w') as plik:
-    # zapisz dane do pliku
-    plik.write('import os\n')
-    plik.write('import tkinter as tk\n')
-    plik.write('from tkinter import messagebox\n')
-    plik.write('from tkinter import scrolledtext\n')
-    plik.write('# Włączanie aktualizacji obu poziomów, a następnie programu')
-# wyświetl komunikat o zakończeniu zapisu
-print(f'Plik {nazwa_pliku} został utworzony i zapisany.')
 
 # KONIEC NOWEJ FUNKCJI
 

64765059d1270c86a5784c3fd81e14a00fb8b802
Ksao0 <128844313+Ksao0@users.noreply.github.com>
2023-04-04 16:53:28 +0200

    Update Aktualizator_aktualizatora.py

--- Aktualizator_aktualizatora.py
+++ Aktualizator_aktualizatora.py
@@ -2,14 +2,14 @@ import os
 import urllib.request
 import subprocess
 
-# ścieżka do pliku Aktualizacja.py w bieżącym folderze
+# Ścieżka do pliku Aktualizacja.py w bieżącym folderze
 path = os.path.join(os.getcwd(), "Aktualizacja.py")
 
-# usuń plik Aktualizacja.py, jeśli istnieje
+# Usuń plik Aktualizacja.py, jeśli istnieje
 if os.path.exists(path):
     os.remove(path)
 print("Rozpoczynanie aktualizacji II poziomu")
-# pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
 url = "https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/Aktualizacja.py"
 urllib.request.urlretrieve(url, path)
 print("Zakończono aktualizację II poziomu")
@@ -19,3 +19,58 @@ Aktualizacja = ["python", "Aktualizacja.py"]
 subprocess.run(Aktualizacja)
 
 print("Zakończono aktualizację I poziomu")
+
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium
+# Pobierz plik Aktualizacja.py z repozytorium

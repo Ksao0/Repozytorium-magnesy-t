4bfb711016d1be8b75337fc1715e9076e7f6394a
Ksao0 <128844313+Ksao0@users.noreply.github.com>
2023-04-04 16:39:08 +0200

    Update Aktualizator_aktualizatora.py

--- Aktualizator_aktualizatora.py
+++ Aktualizator_aktualizatora.py
@@ -2,14 +2,14 @@ import os
 import urllib.request
 import subprocess
 
-# ścieżka do pliku Aktualizator.py w bieżącym folderze
-path = os.path.join(os.getcwd(), "Aktualizator.py")
+# ścieżka do pliku Aktualizacja.py w bieżącym folderze
+path = os.path.join(os.getcwd(), "Aktualizacja.py")
 
-# usuń plik Aktualizator.py, jeśli istnieje
+# usuń plik Aktualizacja.py, jeśli istnieje
 if os.path.exists(path):
     os.remove(path)
 print("Rozpoczynanie aktualizacji II poziomu")
-# pobierz plik Aktualizator.py z repozytorium
+# pobierz plik Aktualizacja.py z repozytorium
 url = "https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/Aktualizacja.py"
 urllib.request.urlretrieve(url, path)
 print("Zakończono aktualizację II poziomu")

7986f6ab5d0e5196efea2b818f23c163d454b2c0
Ksao0 <128844313+Ksao0@users.noreply.github.com>
2023-04-13 16:50:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ from tkinter import scrolledtext
 import datetime
 
 
+# ścieżka do pliku Aktualizacja.py w bieżącym folderze
+path = os.path.join(os.getcwd(), "Aktualizacja.py")
+
+# usuń plik Aktualizacja.py, jeśli istnieje
+if os.path.exists(path):
+    os.remove(path)
+# print("Usunięto plik Aktualizacja.py")
+# pobierz plik Aktualizacja.py z repozytorium
+url = "https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/Aktualizacja.py"
+urllib.request.urlretrieve(url, path)
+# print("Zastąpiono plik Aktualizacja.py")
+
+
 def oblicz_zyski():
 
     # Sprawdzenie, czy plik istnieje i ewentualne jego utworzenie

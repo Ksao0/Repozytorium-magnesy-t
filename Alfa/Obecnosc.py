e11425ecf266ac17d59313a2aaaa21a230b6afa0
Ksao0 <128844313+Ksao0@users.noreply.github.com>
2023-04-29 14:16:10 +0200

    Update Obecnosc.py

--- Alfa/Obecnosc.py
+++ Alfa/Obecnosc.py
@@ -6,14 +6,6 @@ import datetime
 import urllib.request
 import subprocess
 import requests
-import os
-import tkinter as tk
-from tkinter import messagebox
-from tkinter import scrolledtext
-import datetime
-import urllib.request
-import subprocess
-import requests
 
 # pobierz zawartość pliku version.txt z repozytorium na GitHub
 url = 'https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/Alfa/version.txt'
@@ -34,6 +26,8 @@ if version_local == version_online:
     aktul_wersja = 0
 else:
     aktul_wersja = 1
+    print(f'Plik version.txt jest nieaktualny lub uszkodzony.')
+
 
 # pobierz zawartość pliku Aktualizacja.py z repozytorium na GitHub
 url = 'https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/Alfa/Aktualizacja.py'
@@ -54,6 +48,7 @@ if Aktualizacja_local == Aktualizacja_online:
     aktul_Aktualizacja = 0
 else:
     aktul_Aktualizacja = 1
+    print(f'Plik Aktualizacja.py jest nieaktualny lub uszkodzony (aktualizacja możliwa).')
 
 # pobierz zawartość pliku Aktualizator_aktualizatora.py z repozytorium na GitHub
 url = 'https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/Alfa/Aktualizator_aktualizatora.py'
@@ -74,6 +69,7 @@ if Aktualizator_aktualizatora_local == Aktualizator_aktualizatora_online:
     aktul_Aktualizator_aktualizatora = 0
 else:
     aktul_Aktualizator_aktualizatora = 1
+    print(f'Plik Aktualizator_aktualizatora.py jest nieaktualny lub uszkodzony (aktualizacja niemożliwa).')
 
 # pobierz zawartość pliku Obecnosc.py z repozytorium na GitHub
 url = 'https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/Alfa/Obecnosc.py'
@@ -94,6 +90,7 @@ if Obecnosc_local == Obecnosc_online:
     aktul_Obecnosc = 0
 else:
     aktul_Obecnosc = 1
+    print(f'Plik Obecnosc.py jest nieaktualny lub uszkodzony.')
 
 # pobierz zawartość pliku main.py z repozytorium na GitHub
 url = 'https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/Alfa/main.py'
@@ -114,58 +111,41 @@ if main_local == main_online:
     aktul_main = 0
 else:
     aktul_main = 1
-
-# pobierz zawartość pliku version.txt z repozytorium na GitHub
-url = 'https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/Alfa/version.txt'
-response = requests.get(url)
-
-version_online = response.content.decode('utf-8').strip()
-
-# odczytaj zawartość pliku version.txt w twoim programie
-path = os.path.join(os.getcwd(), "version.txt")
-if os.path.exists(path):
-    with open(path, "r", encoding="utf-8") as f:
-        version_local = f.read().strip()
-else:
-    version_local = "BRAK DANYCH"
-
-# wyświetl tylko pierwszą linijkę wersji
-version_local_first_line = version_local.split('\n')[0]
-version_online_first_line = version_online.split('\n')[0]
-
-# porównaj wersje
-print(f'\nWersja na komputerze: {version_local_first_line}')
-print(f'Wersja w repozytorium: {version_online_first_line}')
-if version_local_first_line == version_online_first_line:
-  aktul_wersja = 0
-else:
-  aktul_wersja = 1
+    print(f'Plik main.py jest nieaktualny lub uszkodzony.')
 
 
-  # pobierz zawartość pliku main.py z repozytorium na GitHub
-url = 'https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/Alfa/main.py'
-response = requests.get(url)
+def aktul():
+    os.system('cls')
+    # Ścieżka do pliku Aktualizator_aktualizatora.py w bieżącym folderze
+    path = os.path.join(os.getcwd(), "Aktualizator_aktualizatora.py")
 
-main_online = response.content.decode('utf-8').strip()
+    # Usuń plik Aktualizator_aktualizatora.py, jeśli istnieje
+    if os.path.exists(path):
+        os.remove(path)
+    # Pobierz plik Aktualizator_aktualizatora.py z repozytorium
+    url = "https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/Alfa/Aktualizator_aktualizatora.py"
+    urllib.request.urlretrieve(url, path)
 
-# odczytaj zawartość pliku main.py w twoim programie
-path = os.path.join(os.getcwd(), "main.py")
-if os.path.exists(path):
-    with open(path, "r", encoding="utf-8") as f:
-        main_local = f.read().strip()
-else:
-    main_local = "BRAK DANYCH"
+    print('Naprawa...')
+    Aktualizacja = ["python", "Aktualizator_aktualizatora.py"]
+    subprocess.run(Aktualizacja)
+    print('Zakończono!')
+    print('Uruchom program ponownie, aby wprowadzić zmiany')
 
-# wyświetl tylko pierwszą linijkę wersji
-main_local_first_line = version_local.split('\n')[0]
-main_online_first_line = version_online.split('\n')[0]
 
-# porównaj wersje
-print(f'\nWersja na komputerze: {main_local_first_line}')
-print(f'Wersja w repozytorium: {main_online_first_line}')
-if main_local_first_line == main_online_first_line:
-  aktul_main = 0
-  print(
-else:
-  aktul_main = 1
-  
+def naprawa():
+    naprawa = int(input("Wybór: "))
+    if naprawa == 1:
+        aktul()
+    elif naprawa == 2:
+        print('Program może działać nieprawidłowo. Możesz wykonać te czynności (dostosowano do możliwości):')
+        if aktul_Aktualizator_aktualizatora == 0:
+          print('   Zaktualizować go do najnowszej wersji, jeżeli jeszcze tego nie zrobiono')
+        print('   Ponownie sprawdzić zgodność plików i wybrać opcję naprawy')
+    else:
+        print('Nie ma takiej opcji')
+        naprawa()
+
+
+print('Naprawić błąd? (żadne twoje dane nie zostaną usunęte)\n1: Tak\n2: Nie')
+naprawa()

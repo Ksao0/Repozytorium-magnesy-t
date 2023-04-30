3b2455be03e4d0f02fa6f70095df874c0e4c426d
Ksao0 <128844313+Ksao0@users.noreply.github.com>
2023-04-30 14:50:03 +0200

    Update Aktualizacja.py

--- Aktualizacja.py
+++ Aktualizacja.py
@@ -125,7 +125,7 @@ if wersja_programu == 1:
 
     # KONIEC NOWYCH FUNKCJI
 elif wersja_programu == 2:
-    print(' - import os\n - import tkinter as tk\n - from tkinter import messagebox\n - from tkinter import scrolledtext\n - import datetime\n - import urllib.request\n - import subprocess\n - import requests')
+    print('Zainstaluj poniższe biblioteki komendą pip install NAZWA BIBLIOTEKI w terminalu cmd:\n - os\n - tkinter\n - messagebox\n - scrolledtext\n - datetime\n - urllib\n - subprocess\n - requests')
     input("Naciśnij klawisz Enter, aby potwierdzić, że masz zainstalowane powyższe biblioteki. Nie odpowiadamy za błędy związane z ich nie pobraniem...")
     # ścieżka do pliku main.py w bieżącym folderze
     path = os.path.join(os.getcwd(), "main.py")

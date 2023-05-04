3f2090f147075f3dad11b1299e48d1fb4958133b
Ksao0 <128844313+Ksao0@users.noreply.github.com>
2023-05-04 11:29:23 +0200

    Update main.py

--- Alfa/main.py
+++ Alfa/main.py
@@ -85,7 +85,6 @@ def taj():
                 print('Zaktualizowano!')
                 message = "Program zostanie uruchomiony ponownie"
                 if messagebox.showinfo("Aktualizacja", message):
-                    exit()
             else:
                 exit()
         if version_online_lines[0] == version_local_lines[0] and version_online_lines[1] != "Status: Poprawka wersji" and version_online_lines[2] == version_local_lines[2]:

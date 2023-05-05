7f2c02abcb0e084318a9d332df201e9b274fe22d
Ksao0 <128844313+Ksao0@users.noreply.github.com>
2023-05-05 20:13:50 +0200

    Update main.py

--- Alfa/main.py
+++ Alfa/main.py
@@ -87,6 +87,11 @@ def taj():
                 messagebox.showinfo("Aktualizacja", message)
             else:
                 exit()
+        if version_online_lines[0] == version_local_lines[0] and version_online_lines[1] == "Status: Poprawki B7 zakończone" and version_local_lines[1] != version_online_lines[1]:
+            Aktualizacja = ["python", "WEW.py"]
+            subprocess.run(Aktualizacja)
+            message = "Proces intensywnych zmian w kodzie został zakończony."
+            messagebox.showinfo("Aktualizacja", message)
         if version_online_lines[0] == version_local_lines[0] and version_online_lines[1] != "Status: Poprawka wersji" and version_online_lines[2] == version_local_lines[2]:
             # Nie ma nowszej wersji, więc nie trzeba nic robić
             return

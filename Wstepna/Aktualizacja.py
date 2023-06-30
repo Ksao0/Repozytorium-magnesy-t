63fcc2e0ae0ae8edcef94c3c1bfd3546439f8a84
Ksawo GKW <ksawek8@gmail.com>
2023-06-30 10:43:14 +0200

    Nowy tekst w aktualizacja na stabilną

--- Wstepna/Aktualizacja.py
+++ Wstepna/Aktualizacja.py
@@ -93,7 +93,7 @@ while od_nowa == 1:
                         else:
                             print(Fore.RED + biblioteka)
                 else:
-                    print("Nie ma wymaganych bibliotek na liście.")
+                    print(Fore.GREEN + "Wszystkie biblioteki pobrane.")
 
                 # ścieżka do pliku main.py w bieżącym folderze
                 path = os.path.join(os.getcwd(), "main.py")

115770eebe577fb7bd12ab0534727e023b365676
Ksawo GKW <ksawek8@gmail.com>
2023-08-11 16:26:14 +0200

    AAA

--- Wstepna/main.py
+++ Wstepna/main.py
@@ -743,7 +743,7 @@ def taj():
                     # Użytkownik chce zaktualizować program, więc wykonaj aktualizację
                     Aktualizacja = ["python", "WEW.py"]
                     subprocess.run(Aktualizacja)
-                    zgloszenie_pobrania_nowej_wersji(version_local_lines[0],
+                    zgloszenie_pobrania_nowej_wersji(version_online_lines[0],
                                                      version_local_lines[0])
                     if messagebox.showinfo("Aktualizacja", "Program zostanie uruchomiony ponownie"):
                         restart_program()

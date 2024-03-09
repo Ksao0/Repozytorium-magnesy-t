c05c6ed96a8335aa3e9c155369b13ff1f63dddf1
Ksawo GKW <ksawek8@gmail.com>
2024-03-09 23:50:57 +0100

    AAA

--- Wstepna/Testo/Aktualizator.py
+++ Wstepna/Testo/Aktualizator.py
@@ -12,270 +12,6 @@ import time
 
 from github import Github
 
-class AktualizacjaWatek(QThread):
-    aktualizacja_zakonczona = pyqtSignal(int)
-
-    def __init__(self, urls):
-        super().__init__()
-        self.urls = urls
-
-    def run(self):
-        total_size = sum(self.get_file_size(url) for url in self.urls)
-        current_size = 0
-
-        for url in self.urls:
-            path = os.path.join(os.getcwd(), os.path.basename(url))
-
-            if os.path.exists(path):
-                os.remove(path)
-
-            urllib.request.urlretrieve(url, path)
-
-            current_size += self.get_file_size(url)
-            progress_percentage = int((current_size / total_size) * 100)
-            self.aktualizacja_zakonczona.emit(progress_percentage)
-
-        # Emitowanie sygnału z wartością 100, gdy wszystkie pliki są pobrane
-        self.aktualizacja_zakonczona.emit(100)
-
-    def get_file_size(self, url):
-        with urllib.request.urlopen(url) as response:
-            return int(response.getheader('Content-Length', 0))
-
-class OknoAktualizacji(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        self.urls = [
-            "https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/Wstepna/Testo/Aktualizator.py",
-            "https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/Wstepna/Testo/main.py"
-            # Dodaj tutaj inne URL-e do plików, jeśli są
-        ]
-
-        self.inicjalizuj_ui()
-
-    def inicjalizuj_ui(self):
-        układ = QGridLayout()
-
-        etykieta_info = QLabel('Dostępna jest nowa aktualizacja. Czy chcesz zaktualizować aplikację?')
-        układ.addWidget(etykieta_info, 0, 0, 1, 2)
-
-        self.pasek_postępu = QProgressBar()
-        układ.addWidget(self.pasek_postępu, 1, 0, 1, 2)
-
-        przycisk_tak = QPushButton('Tak')
-        przycisk_anuluj = QPushButton('Anuluj')
-
-        przycisk_tak.clicked.connect(self.rozpocznij_aktualizacje)
-        przycisk_anuluj.clicked.connect(self.anuluj_aktualizacje)
-
-        układ.addWidget(przycisk_tak, 2, 0)
-        układ.addWidget(przycisk_anuluj, 2, 1)
-
-        self.setLayout(układ)
-        self.setWindowTitle('Okno Aktualizatora')
-        self.setGeometry(200, 200, 400, 150)
-
-        self.pasek_postępu.setValue(0)
-        self.watek_aktualizacji = AktualizacjaWatek(self.urls)
-        self.watek_aktualizacji.aktualizacja_zakonczona.connect(self.zakoncz_aktualizacje)
-
-    def rozpocznij_aktualizacje(self):
-        print('Rozpoczęcie aktualizacji...')
-        self.pasek_postępu.setValue(0)
-        self.watek_aktualizacji.start()
-
-    def zakoncz_aktualizacje(self, value):
-        self.pasek_postępu.setValue(value)
-        if value == 100:
-            print('Aktualizacja zakończona.')
-            # Tutaj dodano uruchomienie programu z nowego pliku main.py po zakończeniu aktualizacji
-            subprocess.run(["python", "Aktualizator.py"])
-            self.close()
-            # QCoreApplication.quit()  # Zamknij cały program po zakończeniu aktualizacji
-
-
-    def anuluj_aktualizacje(self):
-        print('Aktualizacja anulowana.')
-        self.watek_aktualizacji.terminate()
-        self.close()
-
-
-
-
-
-class OknoUstawien(QWidget):
-
-    def __init__(self):
-        super().__init__()
-
-        self.inicjalizuj_ui()
-
-    def inicjalizuj_ui(self):
-        # Tworzymy układ siatkowy dla okna ustawień
-        układ = QGridLayout()
-
-        etykieta_ustawien = QLabel(
-            'Podaj ceny kosztów dla jednego pakietu', self)
-        układ.addWidget(etykieta_ustawien, 0, 0, 1, 2)
-
-        etykieta_cena = QLabel('Cena tektury: ', self)
-        układ.addWidget(etykieta_cena, 1, 0, 1, 1)
-
-        pole_cena_tektura = QDoubleSpinBox(self)
-        układ.addWidget(pole_cena_tektura, 1, 1, 1, 1)
-
-        etykieta_cena = QLabel('Cena nadruku: ', self)
-        układ.addWidget(etykieta_cena, 2, 0, 1, 1)
-
-        pole_cena_nadruk = QDoubleSpinBox(self)
-        układ.addWidget(pole_cena_nadruk, 2, 1, 1, 1)
-
-        etykieta_cena = QLabel('Cena folii: ', self)
-        układ.addWidget(etykieta_cena, 3, 0, 1, 1)
-
-        pole_cena_folia = QDoubleSpinBox(self)
-        układ.addWidget(pole_cena_folia, 3, 1, 1, 1)
-
-        etykieta_cena = QLabel('Cena woreczków: ', self)
-        układ.addWidget(etykieta_cena, 4, 0, 1, 1)
-
-        pole_cena_woreczki = QDoubleSpinBox(self)
-        układ.addWidget(pole_cena_woreczki, 4, 1, 1, 1)
-
-        button_zapisz1 = QPushButton('Ustaw domyślne ceny', self)
-        button_zapisz1.clicked.connect(lambda: self.zmien_ceny(None, None, None, None, etykieta_cena_tektura,
-                                                               etykieta_cena_nadruk, etykieta_cena_folia, etykieta_cena_woreczki))  # Połącz przycisk z funkcją
-        układ.addWidget(button_zapisz1, 5, 0, 1, 1)
-
-        button_zapisz2 = QPushButton('Zapisz zmiany', self)
-        button_zapisz2.clicked.connect(lambda: self.zmien_ceny(
-            pole_cena_tektura, pole_cena_nadruk, pole_cena_folia, pole_cena_woreczki, etykieta_cena_tektura, etykieta_cena_nadruk, etykieta_cena_folia, etykieta_cena_woreczki))  # Połącz przycisk z funkcją
-        układ.addWidget(button_zapisz2, 5, 1, 1, 1)
-
-        etykieta_cena_tektura = QLabel('Używana cena tektury: ', self)
-        układ.addWidget(etykieta_cena_tektura, 6, 0, 1, 2)
-
-        etykieta_cena_nadruk = QLabel('Używana cena nadruku: ', self)
-        układ.addWidget(etykieta_cena_nadruk, 7, 0, 1, 2)
-
-        etykieta_cena_folia = QLabel('Używana cena folii: ', self)
-        układ.addWidget(etykieta_cena_folia, 8, 0, 1, 2)
-
-        etykieta_cena_woreczki = QLabel('Używana cena woreczków: ', self)
-        układ.addWidget(etykieta_cena_woreczki, 9, 0, 1, 2)
-
-        # Dodaj więcej elementów do okna ustawień, jeśli jest to konieczne
-
-        # Ustawiamy układ dla okna ustawień
-        self.setLayout(układ)
-
-        # Ustawiamy tytuł i rozmiar okna ustawień
-        self.setWindowTitle('Okno Ustawień')
-        self.setGeometry(900, 300, 400, 300)
-
-        # # Pobieranie kosztów z pliku
-        path = os.path.join(os.getcwd(), "Ceny.txt")
-
-        # zapisz zawartość pliku Ceny.txt do zmiennej teraz_ceny
-        if os.path.exists(path):
-            with open(path, "r", encoding='utf-8') as f:
-                teraz_ceny = f.read()
-        else:
-            teraz_ceny = "13\n35\n18\n11"
-
-        # Check if teraz_ceny is not empty before attempting to split and convert to float
-        if teraz_ceny.strip():  # strip removes leading and trailing whitespaces
-            ceny_tektura = round(float(teraz_ceny.split('\n')[0]), 2)
-            ceny_nadruk = round(float(teraz_ceny.split('\n')[1]), 2)
-            ceny_foliamg = round(float(teraz_ceny.split('\n')[2]), 2)
-            ceny_woreczkipp = round(float(teraz_ceny.split('\n')[3]), 2)
-        else:
-
-            ceny_tektura = 0.0
-            ceny_nadruk = 0.0
-            ceny_foliamg = 0.0
-            ceny_woreczkipp = 0.0
-
-        etykieta_cena_tektura.setText(
-            f'Używana cena tektury: {ceny_tektura} zł')
-        etykieta_cena_nadruk.setText(f'Używana cena nadruku: {ceny_nadruk} zł')
-        etykieta_cena_folia.setText(f'Używana cena folii: {ceny_foliamg} zł')
-        etykieta_cena_woreczki.setText(
-            f'Używana cena woreczków: {ceny_woreczkipp} zł')
-
-    def zmien_ceny(self, pole_cena_tektura, pole_cena_nadruk, pole_cena_folia, pole_cena_woreczki, etykieta_cena_tektura, etykieta_cena_nadruk, etykieta_cena_folia, etykieta_cena_woreczki):
-        try:
-            if pole_cena_tektura:
-                ceny_tektura = pole_cena_tektura.value()
-                ceny_nadruk = pole_cena_nadruk.value()
-                ceny_foliamg = pole_cena_folia.value()
-                ceny_woreczkipp = pole_cena_woreczki.value()
-            else:
-                ceny_tektura = '13'
-                ceny_nadruk = '35'
-                ceny_foliamg = '18'
-                ceny_woreczkipp = '11'
-
-            ceny_tektura = str(ceny_tektura)
-            ceny_nadruk = str(ceny_nadruk)
-            ceny_foliamg = str(ceny_foliamg)
-            ceny_woreczkipp = str(ceny_woreczkipp)
-
-            path = os.path.join(os.getcwd(), "Ceny.txt")
-
-            if os.path.exists(path):
-                os.remove(path)
-
-            with open("Ceny.txt", "a", encoding='utf-8') as plik:
-                plik.write(ceny_tektura)
-                plik.write('\n')
-                plik.write(ceny_nadruk)
-                plik.write('\n')
-                plik.write(ceny_foliamg)
-                plik.write('\n')
-                plik.write(ceny_woreczkipp)
-            if not os.path.isfile("Ceny.txt"):
-                open("Ceny.txt", "w", encoding='utf-8').close()
-                plik.write(ceny_tektura)
-                plik.write('\n')
-                plik.write(ceny_nadruk)
-                plik.write('\n')
-                plik.write(ceny_foliamg)
-                plik.write('\n')
-                plik.write(ceny_woreczkipp)
-
-            etykieta_cena_tektura.setText(
-                f'Używana cena tektury: {ceny_tektura}')
-            etykieta_cena_nadruk.setText(
-                f'Używana cena nadruku: {ceny_nadruk}')
-            etykieta_cena_folia.setText(f'Używana cena folii: {ceny_foliamg}')
-            etykieta_cena_woreczki.setText(
-                f'Używana cena woreczków: {ceny_woreczkipp}')
-            # Zapisz wynik obliczeń do pliku Zapisy.txt
-
-            path = os.path.join(os.getcwd(), "Zapisy.txt")
-
-            if os.path.exists(path):
-                os.remove(path)
-
-            wszystkie_zmiany_cen = f" * ** *** ** * ZMIANA CEN KOSZTÓW * ** *** ** *\n" + \
-                f"    Nowa cena tektury: {ceny_tektura} zł\n" + \
-                f"    Nowa cena nadruku: {ceny_nadruk} zł\n" + \
-                f"    Nowa cena folii: {ceny_foliamg} zł\n" + \
-                f"    Nowa cena woreczków: {ceny_woreczkipp} zł\n\n"
-
-            aktualna_zawartosc = text_edit_historia.toPlainText()
-            text_edit_historia.setPlainText(
-                wszystkie_zmiany_cen + aktualna_zawartosc)
-
-            with open("Zapisy.txt", "a", encoding='utf-8') as plik:
-                plik.write(f"{aktualna_zawartosc}")
-
-        except Exception as e:
-            print(e)
-            return
-
 
 class ZaawansowaneOkno(QWidget):
     def __init__(self):
@@ -296,13 +32,9 @@ class ZaawansowaneOkno(QWidget):
 
         button_ustawienia = QPushButton('Ustawienia', self)
         układ.addWidget(button_ustawienia, 0, 4, 1, 1)
-        button_ustawienia.clicked.connect(
-            self.pokaz_ustawienia)  # Połącz przycisk z funkcją
 
         button_opinie = QPushButton('Aktualizuj', self)
         układ.addWidget(button_opinie, 0, 3, 1, 1)
-        button_opinie.clicked.connect(
-            self.pokaz_aktualizator)  # Poprawione podłączenie do funkcji
 
         button_usun_zapisy = QPushButton('Usuń zapisy', self)
         układ.addWidget(button_usun_zapisy, 0, 5, 1, 1)
@@ -352,7 +84,7 @@ class ZaawansowaneOkno(QWidget):
         self.setLayout(układ)
 
         # Ustawiamy tytuł i rozmiar głównego okna
-        self.setWindowTitle('Aktualizator ')
+        self.setWindowTitle('Magnesy v2')
         self.setGeometry(300, 300, 600, 400)
 
         # Arkusz stylów
@@ -469,6 +201,71 @@ class ZaawansowaneOkno(QWidget):
         # Ustawienie arkusza stylów
         app.setStyleSheet(dark_stylesheet)
 
+    def wykasuj_zapisy(self, text_edit_historia):
+        try:
+            if messagebox.askyesno('Kasowanie historii', 'Czy na pewno chcesz usunąć poprzednie obliczenia?'):
+                # Ścieżka do pliku Zapisy.txt w bieżącym folderze
+                path = os.path.join(os.getcwd(), "Zapisy.txt")
+
+                # Usuń plik Zapisy.txt, jeśli istnieje
+                if os.path.exists(path):
+                    os.remove(path)
+                    with open('Zapisy.txt', mode='w', encoding='utf-8') as file:
+                        file.write('')
+
+                text_edit_historia.setPlainText('')
+        except:
+            messagebox.showinfo(
+                'Brak pliku', "Nie masz jeszcze historii zapisów")
+
+    def oblicz_i_zapisz(self, pole_ilosc, pole_cena, text_edit_historia, etykieta_zarobisz, etykieta_wydasz, etykieta_calkowita_wartosc_pakietow):
+        liczba_pakietow = pole_ilosc.value()
+        cena_za_magnes = pole_cena.value()
+
+        now = datetime.datetime.now()
+
+        data_obliczenia = now.strftime("%d.%m.%Y %H:%M:%S")
+
+        # # Pobieranie kosztów z pliku
+        path = os.path.join(os.getcwd(), "Ceny.txt")
+
+        # zapisz zawartość pliku Ceny.txt do zmiennej teraz_ceny
+        if os.path.exists(path):
+            with open(path, "r", encoding='utf-8') as f:
+                teraz_ceny = f.read()
+        else:
+            teraz_ceny = "13\n35\n18\n11"
+
+        ceny_tektura = round(float(teraz_ceny.split('\n')[0]), 2)
+        ceny_nadruk = round(float(teraz_ceny.split('\n')[1]), 2)
+        ceny_foliamg = round(float(teraz_ceny.split('\n')[2]), 2)
+        ceny_woreczkipp = round(float(teraz_ceny.split('\n')[3]), 2)
+
+        magnesy_w_pakiecie = liczba_pakietow * 224
+        cena_za_pakiet = cena_za_magnes * 224
+        razem = cena_za_pakiet * liczba_pakietow
+
+        tektura = ceny_tektura * liczba_pakietow
+        nadruk = ceny_nadruk * liczba_pakietow
+        foliamg = ceny_foliamg * liczba_pakietow
+        woreczkipp = ceny_woreczkipp * liczba_pakietow
+
+        koszty = tektura + nadruk + foliamg + woreczkipp
+        bilans = razem - koszty
+
+        wyniki = f"Data: {data_obliczenia}\n\nLiczba pakietów: {liczba_pakietow} szt.\nLiczba magnesów: {magnesy_w_pakiecie} szt.\nCena za 1 magnes: {cena_za_magnes:.2f}zł\nJeden pakiet to: {cena_za_pakiet:.2f} zł\nKoszty: {koszty:.2f} zł\nZysk sprzedaży: {bilans:.2f} zł\nCena za wszystkie pakiety: {razem:.2f} zł\n\n"
+        aktualna_zawartosc = text_edit_historia.toPlainText()
+        text_edit_historia.setPlainText(wyniki + aktualna_zawartosc)
+
+        etykieta_zarobisz.setText(f'Zarobisz: {bilans:.2f} zł')
+        etykieta_wydasz.setText(f'Wydasz: {koszty:.2f} zł')
+
+        etykieta_calkowita_wartosc_pakietow.setText(
+            f'Całkowita wartość pakietów: {razem:.2f} zł')
+
+        # Zapisz wynik obliczeń do pliku Zapisy.txt
+        with open('Zapisy.txt', mode='a', encoding='utf-8') as file:
+            file.write(wyniki)
 
 
 if __name__ == '__main__':

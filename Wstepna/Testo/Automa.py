74c9ec98776d6ec2b06a6cea3cbb827aee505fdb
Ksawo GKW <ksawek8@gmail.com>
2024-04-15 17:14:18 +0200

    AAA

--- Wstepna/Testo/Automa.py
+++ Wstepna/Testo/Automa.py
@@ -1,6 +1,5 @@
 import os
 import requests
-from PyQt5.QtCore import QCoreApplication, QThread, pyqtSignal
 import ctypes
 import sys
 from colorama import Fore, Style
@@ -11,11 +10,8 @@ ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
 try:
     print(f'{Fore.LIGHTBLACK_EX}Aktualizowanie magnesów\n\nAby wyłączyć tę opcję usuń ją z autostaru')
 
-    class Automa(QThread):
-        aktualizacja_zakonczona = pyqtSignal(int)
-
+    class Automa:
         def __init__(self, urls, folder_path, version_txt_path):
-            super().__init__()
             self.urls = urls
             self.folder_path = folder_path
             self.version_txt_path = version_txt_path
@@ -24,14 +20,12 @@ try:
             local_version_content = self.read_local_version_txt()
             if local_version_content is None:
                 print("Nie udało się odczytać pliku version.txt na komputerze.")
-                QCoreApplication.quit()
-                return
+                sys.exit(1)
 
             remote_version_content = self.read_remote_version_txt()
             if remote_version_content is None:
                 print("Nie udało się pobrać pliku version.txt z repozytorium.")
-                QCoreApplication.quit()
-                return
+                sys.exit(1)
 
             if local_version_content != remote_version_content:
                 total_files = len(self.urls)
@@ -43,17 +37,19 @@ try:
                         response.headers.get('content-length', 0))
                     block_size = 1024  # 1 KB
                     progress_bar = 0
+                    previous_percent = 0  # Przechowuje poprzedni procent postępu
                     with open(file_name, 'wb') as file:
                         for data in response.iter_content(block_size):
                             file.write(data)
                             progress_bar += len(data)
-                            percent = progress_bar * 100 / total_size_in_bytes
-                            self.aktualizacja_zakonczona.emit(
-                                int(i + percent / total_files))
+                            percent = min(progress_bar * 100 / total_size_in_bytes, 100)  # Ograniczenie do maksymalnie 100%
+                            if int(percent) != previous_percent:  # Wyświetl tylko, gdy postęp się zmienia
+                                print(f"Pobrano {file_name}: {percent:.2f}%")
+                                previous_percent = int(percent)  # Aktualizuj poprzedni procent
             else:
                 print(
                     "Plik version.txt na komputerze jest aktualny. Nie ma potrzeby aktualizacji.")
-                QCoreApplication.quit()
+                sys.exit(0)
 
         def read_local_version_txt(self):
             try:
@@ -78,9 +74,6 @@ try:
             break
     else:
         print("Nie znaleziono wymaganych plików/folderów na pulpicie.")
-        import messagebox
-        messagebox.showerror(
-            'Autostart - magnesy', "Niestety nie można znaleźć na pulpicie folderu z programem, jeśli go usunąłeś lub przeniosłeś gdzieś indziej - usuń Magnesy-update.bat z autostartu")
         sys.exit(1)
 
     version_txt_path = os.path.join(folder_path, "version.txt")
@@ -92,9 +85,6 @@ try:
         print("Brak połączenia z internetem.")
         sys.exit(1)
 
-    # Użycie:
-    app = QCoreApplication(sys.argv)
-
     # Ścieżka do pliku lista.txt w repozytorium
     lista_txt_url = "https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/Wstepna/Testo/lista.txt"
     lista_txt_content = requests.get(lista_txt_url).text
@@ -102,10 +92,7 @@ try:
             for line in lista_txt_content.split('\n') if line.strip()]
 
     automa = Automa(urls, folder_path, version_txt_path)
-    automa.aktualizacja_zakonczona.connect(lambda value: app.quit())
-    automa.start()
-
-    sys.exit(app.exec_())
+    automa.run()
 
 except Exception as e:
     print(f"Wystąpił błąd: {e}")

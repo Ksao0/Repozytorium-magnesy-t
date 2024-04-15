64743ab858c9527bd74b6d1929533d027ac4c456
Ksawo GKW <ksawek8@gmail.com>
2024-04-15 16:17:19 +0200

    AAA

--- Wstepna/Testo/Automa.py
+++ Wstepna/Testo/Automa.py
@@ -1,73 +1,45 @@
-import os
 import requests
-from PyQt5.QtCore import QCoreApplication, QThread, pyqtSignal, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import QCoreApplication, QThread, pyqtSignal
 import ctypes
 import sys
+import threading
 from colorama import Fore, Style
-
 # Minimalizowanie cmd
 ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
 
 try:
+    print(f'{Fore.LIGHTBLACK_EX}Aktualizowanie magnesów\n\nAby wyłączyć tę opcję usuń ją z autostaru')
     class Automa(QThread):
         aktualizacja_zakonczona = pyqtSignal(int)
 
-        def __init__(self, urls, folder_path):
+        def __init__(self, urls):
             super().__init__()
             self.urls = urls
-            self.folder_path = folder_path
 
         def run(self):
-            total_size = 0
-            for url in self.urls:
-                total_size += self.get_file_size(url)
-            total_downloaded = 0
-            for url in self.urls:
-                file_name = os.path.join(self.folder_path, url.split('/')[-1])
+            total_files = len(self.urls)
+            for i, url in enumerate(self.urls):
+                file_name = url.split('/')[-1]
                 response = requests.get(url, stream=True)
-                total_size_in_bytes = self.get_file_size(url)
+                total_size_in_bytes = int(
+                    response.headers.get('content-length', 0))
                 block_size = 1024  # 1 KB
+                progress_bar = 0
                 with open(file_name, 'wb') as file:
                     for data in response.iter_content(block_size):
                         file.write(data)
-                        total_downloaded += len(data)
-                        percent = total_downloaded * 100 / total_size
-                        self.aktualizacja_zakonczona.emit(int(percent))
-
-        def get_file_size(self, url):
-            response = requests.head(url)
-            return int(response.headers.get('content-length', 0))
+                        progress_bar += len(data)
+                        percent = progress_bar * 100 / total_size_in_bytes
+                        self.aktualizacja_zakonczona.emit(
+                            int(i + percent / total_files))
 
     def read_urls_from_file(file_path):
         with open(file_path, 'r') as file:
             urls = [line.strip() for line in file.readlines()]
         return urls
 
-    class MainWindow(QMainWindow):
-        def __init__(self):
-            super().__init__()
-            self.setWindowTitle("Aktualizacja plików (magnesy)")
-            self.setFixedSize(400, 70)
-            self.setStyleSheet(
-                "QWidget { background-color: #1B0C1A; color: #F2F2F2; selection-color: #40535b; selection-background-color: #441d32; }"
-                "QStatusBar { background-color: #333333; color: #FFFFFF; }"
-            )
-            self.show()
-
-    # Szukanie folderu na pulpicie zawierającego wymagane pliki
-    desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
-    for root, dirs, files in os.walk(desktop_path):
-        if "main2.py" in files and "version.txt" in files and "rei" in dirs:
-            folder_path = root
-            break
-    else:
-        print("Nie znaleziono wymaganych plików/folderów na pulpicie.")
-        sys.exit(1)
-
-    # Kontynuowanie działania programu, jeśli znaleziono odpowiedni folder
-    app = QApplication(sys.argv)
-    main_window = MainWindow()
+    # Użycie:
+    app = QCoreApplication(sys.argv)
 
     # Ścieżka do pliku lista.txt w repozytorium
     lista_txt_url = "https://raw.githubusercontent.com/Ksao0/Repozytorium-magnesy-t/main/Wstepna/Testo/lista.txt"
@@ -75,12 +47,10 @@ try:
     urls = [line.strip()
             for line in lista_txt_content.split('\n') if line.strip()]
 
-    automa = Automa(urls, folder_path)
-    automa.aktualizacja_zakonczona.connect(
-        lambda value: app.quit())  # Zamyka aplikację po pobraniu
+    automa = Automa(urls)
+    automa.aktualizacja_zakonczona.connect(lambda value: app.quit())
     automa.start()
 
     sys.exit(app.exec_())
-
-except Exception as e:
-    print(f"Wystąpił błąd: {e}")
+except:
+    pass
